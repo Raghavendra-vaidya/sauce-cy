7c6a3e6283ed57352e964aecfa27a0de860e3cda
Raghavendra Vaidya <rkvaidya58@gmail.com>
2022-07-31 11:30:48 +0530

    add circle ci jobs :rocket:

--- cypress/integration/e2e/login.e2e.spec.js
+++ cypress/integration/e2e/login.e2e.spec.js
@@ -1,13 +1,15 @@
+const { expect } = require("chai");
+
 describe('Login screen e2e tests', () => 
 {
     beforeEach(() => {
-        
+        console.log("RUNNING LOGIN TESTS");    
     });
     
     
 
     it('Lgin as successful user', () => {
-        
+        expect(true).to.equal(true)
     });
 
 });
