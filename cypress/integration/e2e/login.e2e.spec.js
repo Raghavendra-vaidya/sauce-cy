e17e59ed8e131f5af3015ee55484e1be86c25e47
Raghavendra Vaidya <rkvaidya58@gmail.com>
2022-08-11 21:46:17 +0530

    add login e2e test :white_check_mark:

--- cypress/integration/e2e/login.e2e.spec.js
+++ cypress/integration/e2e/login.e2e.spec.js
@@ -1,13 +1,17 @@
+import {LoginPage} from '../../pages/login.page';
+import {ProductsPage} from '../../pages/products.page';
+
 describe('Login screen e2e tests', () => 
-{
-    beforeEach(() => {
-        
-    });
-    
-    
+{   
+     const login =  new LoginPage();
+     const products = new ProductsPage();
 
     it('Lgin as successful user', () => {
-        
+        login.typeUserName("standard_user");
+        login.typePassword("secret_sauce");
+        login.clickLogin();
+        products.getProductsTitle().should('be.visible');
+        products.getSortElement().should('be.visible');
     });
 
 });
