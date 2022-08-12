7494d46be3c04c99e3c7149c89c493154125b0c7
Raghavendra Vaidya <rkvaidya58@gmail.com>
2022-08-12 23:31:04 +0530

    add test for login e2e, add shopping cart page and constants :zap:

--- cypress/identifiers/index.js
+++ cypress/identifiers/index.js
@@ -1,2 +1,3 @@
 export {LOGIN_PAGE_CONSTANTS} from '../identifiers/loginConstants';
 export {PRODUCT_PAGE_CONSTANTS} from '../identifiers/productsConstants';
+export {SHOPPING_CART_CONSTANTS} from '../identifiers/shoppingcartConstants';
---
+++ cypress/identifiers/shoppingcartConstants.js
@@ -0,0 +1,6 @@
+
+export const SHOPPING_CART_CONSTANTS = {
+    itemTitle: '.inventory_item_name',
+    continueShopping:'[data-test="continue-shopping"]',
+    checkout:'[data-test="checkout"]'
+}
