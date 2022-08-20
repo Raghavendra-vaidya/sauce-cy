ac72a377b0cfeac28a45732247501f5fbc0cd264
Raghavendra Vaidya <rkvaidya58@gmail.com>
2022-08-20 18:15:45 +0530

    update product tests :zap:

--- cypress/identifiers/productsConstants.js
+++ cypress/identifiers/productsConstants.js
@@ -6,5 +6,6 @@ export const PRODUCT_PAGE_CONSTANTS = {
     backpackAddToCard: '[id="add-to-cart-sauce-labs-backpack"]',
     backpackRemoveFromCart: '[data-test="remove-sauce-labs-backpack"]',
     shoppingcart:'a[class = "shopping_cart_link"]',
+    product1Div: '.inventory_item_name'
 
 }
--- cypress/integration/integ/products.spec.js
+++ cypress/integration/integ/products.spec.js
@@ -1,6 +1,13 @@
 import {ProductsPage} from '../../pages/products.page';
 import {LoginPage} from '../../pages/login.page';
-import { Context } from 'mocha';
+import { PRODUCT_PAGE_CONSTANTS} from '../../identifiers';
+
+const { 
+   productsTitle,
+   hamburgerMenu, 
+   shoppingcart, 
+   product1Div } = PRODUCT_PAGE_CONSTANTS;
+
 
 describe('Product page tests', () => {
 
@@ -15,8 +22,13 @@ describe('Product page tests', () => {
 
      context('Static text related tests', () =>{
 
-        it('', () => {
-            
+        it('Verify title of each product', () => {
+            cy.get(product1Div).eq(0).should('have.text','Sauce Labs Backpack');
+            cy.get(product1Div).eq(1).should('have.text','Sauce Labs Bike Light');
+            cy.get(product1Div).eq(2).should('have.text','Sauce Labs Bolt T-Shirt');
+            cy.get(product1Div).eq(3).should('have.text','Sauce Labs Fleece Jacket');
+            cy.get(product1Div).eq(4).should('have.text','Sauce Labs Onesie');
+            cy.get(product1Div).eq(5).contains('T-Shirt (Red)');
         });
 
 
